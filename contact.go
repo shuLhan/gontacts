f418068b8871475152c11ff273a2bfc7b682e0b5
M. Shulhan <ms@kilabit.info>
2017-08-14 17:10:45 +0700

    contact: add field Notes and enhance parsing on set birthday

--- contact.go
+++ contact.go
@@ -15,19 +15,28 @@ type Contact struct {
 	Emails      []Email
 	Phones      []Phone
 	Links       []string
+	Notes       []string
 	Company     string
 	JobTitle    string
 }
 
 //
-// SetBirthday will set contact birthday from string format "YYYY-MM-DD".
+// SetBirthday will set contact birthday from string format "YYYY-MM-DD" or
+// "YYYY-MM-DDTHH:MM:SSZ".
+//
+// (1) Split by zone first, and then
+// (2) split the date.
 //
 func (contact *Contact) SetBirthday(dateStr string) {
 	if dateStr == "" {
 		return
 	}
 
-	dates := strings.Split(dateStr, "-")
+	// (1)
+	dateTimeZone := strings.Split(dateStr, "T")
+
+	// (2)
+	dates := strings.Split(dateTimeZone[0], "-")
 	if len(dates) != 3 {
 		return
 	}
